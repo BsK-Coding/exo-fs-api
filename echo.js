2bafa912f729a7198d1df06c275e7f82f98f0c8c
BsK-Coding <blackshark.blog@gmail.com>
2021-03-25 17:40:03 +0100

    maj

--- echo.js
+++ echo.js
@@ -5,9 +5,6 @@ hello hard fork
 
 /* Déclaration des arguments */
 // On entreras les paramètres suivants: 1-node 2-echo.js 3-hello 4-hard 5- fork
-let arg1 = process.argv[2]
-let arg2 = process.argv[3]
-let arg3 = process.argv[4]
 
 /* check des arguments */
 // Pas besoin de vérifier si les arguments sont des nombres car les arguments entré seront forcément des strings
@@ -16,4 +13,5 @@ if (process.argv.length !== 5) {
   process.exit(1)
 }
 
-console.log(`${arg1} ${arg2} ${arg3}`)
+
+console.log(`${process.argv[2]} ${process.argv[3]} ${process.argv[4]}`)
